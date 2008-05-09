4144ae0a8e55e8d03ccc78ca91302e1dbe998450
Ryan Bates <ryan@railscasts.com>
2008-05-09 10:22:59 -0700

    fixing stubbing behavior

--- rails_generators/nifty_scaffold/templates/action_specs/create.rb
+++ rails_generators/nifty_scaffold/templates/action_specs/create.rb
@@ -1,11 +1,11 @@
   it "create action should render new template when model is invalid" do
-    <%= class_name %>.any_instance.stub(:valid? => false)
+    <%= class_name %>.any_instance.stubs(:valid?).returns(false)
     post :create
     response.should render_template(:new)
   end
   
   it "create action should redirect to index action when model is valid" do
-    <%= class_name %>.any_instance.stub(:valid? => true)
+    <%= class_name %>.any_instance.stubs(:valid?).returns(true)
     post :create
     response.should redirect_to(<%= item_path_for_spec %>)
   end
--- rails_generators/nifty_scaffold/templates/action_tests/create.rb
+++ rails_generators/nifty_scaffold/templates/action_tests/create.rb
@@ -1,11 +1,11 @@
   def test_create_invalid
-    <%= class_name %>.any_instance.stub(:valid? => false)
+    <%= class_name %>.any_instance.stubs(:valid?).returns(false)
     post :create
     assert_template :new
   end
   
   def test_create_valid
-    <%= class_name %>.any_instance.stub(:valid? => true)
+    <%= class_name %>.any_instance.stubs(:valid?).returns(true)
     post :create
     assert_redirected_to <%= item_path_for_test %>
   end
