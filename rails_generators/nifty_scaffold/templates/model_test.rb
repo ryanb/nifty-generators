21a438f33cbe10a1657589141829e31d83160af5
Ryan Bates <ryan@railscasts.com>
2008-07-21 08:55:32 -0700

    using same logic as model spec in model test for scaffold

--- rails_generators/nifty_scaffold/templates/model_test.rb
+++ rails_generators/nifty_scaffold/templates/model_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class <%= class_name %>Test < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_be_valid
+    assert <%= class_name %>.new.valid?
   end
 end
