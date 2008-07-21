5a6528f5debde05a6822cc8d8e79f156f7ec7b6d
Ryan Bates <ryan@railscasts.com>
2008-07-21 08:52:56 -0700

    simplifying model spec generated by scaffold

--- rails_generators/nifty_scaffold/templates/model_spec.rb
+++ rails_generators/nifty_scaffold/templates/model_spec.rb
@@ -1,11 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe <%= class_name %> do
-  before(:each) do
-    @<%= singular_name %> = <%= class_name %>.new
-  end
-  
   it "should be valid" do
-    @<%= singular_name %>.should be_valid
+    <%= class_name %>.new.should be_valid
   end
 end
