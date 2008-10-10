0b016bff425537140504d801ed107a7ec59fae6a
Ryan Bates <ryan@railscasts.com>
2008-10-10 09:46:21 -0700

    nifty_authentication: adding more usage documentation with examples.

--- rails_generators/nifty_authentication/templates/authentication.rb
+++ rails_generators/nifty_authentication/templates/authentication.rb
@@ -1,3 +1,19 @@
+# This module is included in your application controller which makes
+# several methods available to all controllers and views. Here's a
+# common example you might add to your application layout file.
+# 
+#   <% if logged_in? %>
+#     Welcome <%= current_user.username %>! Not you?
+#     <%= link_to "Log out", logout_path %>
+#   <% else %>
+#     <%= link_to "Sign up", signup_path %> or
+#     <%= link_to "log in", login_path %>.
+#   <% end %>
+# 
+# You can also restrict unregistered users from accessing a controller using
+# a before filter. For example.
+# 
+#   before_filter :login_required, :except => [:index, :show]
 module Authentication
   def self.included(controller)
     controller.send :helper_method, :current_<%= user_singular_name %>, :logged_in?
