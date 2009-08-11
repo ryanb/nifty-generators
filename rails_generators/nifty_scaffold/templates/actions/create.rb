95c59fd8068f65d8cd561df62bbb0bd7a268d6ee
Aaron <wallywheels@gmail.com>
2009-08-11 17:19:16 -0500

    Modified flash notices for create, update & destroy to properly inflect multi-word class names

--- rails_generators/nifty_scaffold/templates/actions/create.rb
+++ rails_generators/nifty_scaffold/templates/actions/create.rb
@@ -1,7 +1,7 @@
   def create
     @<%= singular_name %> = <%= class_name %>.new(params[:<%= singular_name %>])
     if @<%= singular_name %>.save
-      flash[:notice] = "Successfully created <%= name.humanize.downcase %>."
+      flash[:notice] = "Successfully created <%= name.underscore.humanize.downcase %>."
       redirect_to <%= item_path('url') %>
     else
       render :action => 'new'
--- rails_generators/nifty_scaffold/templates/actions/update.rb
+++ rails_generators/nifty_scaffold/templates/actions/update.rb
@@ -1,7 +1,7 @@
   def update
     @<%= singular_name %> = <%= class_name %>.find(params[:id])
     if @<%= singular_name %>.update_attributes(params[:<%= singular_name %>])
-      flash[:notice] = "Successfully updated <%= name.humanize.downcase %>."
+      flash[:notice] = "Successfully updated <%= name.underscore.humanize.downcase %>."
       redirect_to <%= item_path('url') %>
     else
       render :action => 'edit'
