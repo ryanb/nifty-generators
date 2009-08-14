8aed0bca5d4a91bb10913ff178e525c7ee6e4bfb
Ryan Bates <ryan@railscasts.com>
2009-08-14 11:36:03 -0700

    map arguments directly in stylesheet and javascript includes instead of converting to a string - closes #14

--- rails_generators/nifty_layout/templates/helper.rb
+++ rails_generators/nifty_layout/templates/helper.rb
@@ -13,11 +13,10 @@ module LayoutHelper
   end
   
   def stylesheet(*args)
-    content_for(:head) { stylesheet_link_tag(*args.map(&:to_s)) }
+    content_for(:head) { stylesheet_link_tag(*args) }
   end
   
   def javascript(*args)
-    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
     content_for(:head) { javascript_include_tag(*args) }
   end
 end
