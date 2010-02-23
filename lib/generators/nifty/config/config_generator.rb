f7b63a48d2dbf43cabbbef3e1dffa6c057a402a9
Ryan Bates <ryan@railscasts.com>
2010-02-22 16:42:12 -0800

    fixing name of config generator method

--- lib/generators/nifty/config/config_generator.rb
+++ lib/generators/nifty/config/config_generator.rb
@@ -5,7 +5,7 @@ module Nifty
     class ConfigGenerator < Base
       argument :config_name, :type => :string, :default => 'app', :banner => 'config_name'
       
-      def create_layout
+      def create_config
         template "load_config.rb", "config/initializers/load_#{file_name}_config.rb"
         copy_file "config.yml", "config/#{file_name}_config.yml"
       end
