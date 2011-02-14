43ff103785150e86681bc76f37a064eaed50326b
Ryan Bates <ryan@railscasts.com>
2011-02-14 10:42:15 -0800

    fixing spacing issue in nifty scaffold model generator

--- lib/generators/nifty/scaffold/templates/model.rb
+++ lib/generators/nifty/scaffold/templates/model.rb
@@ -1,4 +1,4 @@
 class <%= class_name %> < ActiveRecord::Base
-  <%= "set_table_name :#{table_name}\n" if table_name -%>
+<%= "  set_table_name :#{table_name}\n" if table_name -%>
   attr_accessible <%= model_attributes.map { |a| ":#{a.name}" }.join(", ") %>
 end
