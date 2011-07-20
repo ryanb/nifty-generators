38099697704b6eb8cade9fb901acfde2ad6381a8
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-07-20 17:48:34 +0200

    copies assets to Rails 3.1 specific directory (if on Rails 3.1), does not overwrite application.css on Rails 3.1

--- lib/generators/nifty/layout/layout_generator.rb
+++ lib/generators/nifty/layout/layout_generator.rb
@@ -9,15 +9,22 @@ module Nifty
       class_option :scss, :desc => 'Generate HAML for view, and SCSS for stylesheet.', :type => :boolean
 
       def create_layout
+        style_name = file_name
+        style_path = 'public'
+        if (Rails.version =~ /^3\.1/) != nil
+          # For Rails 3.1 and its assets pipeline we don't want to overwrite application.css
+          style_name = 'nifty_layout' if file_name == 'application'
+          style_path = 'app/assets'
+        end
         if options.haml?
           template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
-          copy_file 'stylesheet.sass', "app/assets/stylesheets/#{file_name}.sass"
+          copy_file 'stylesheet.sass', "#{style_path}/stylesheets/#{style_name}.sass"
         elsif options.scss?
           template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
-          copy_file 'stylesheet.css.scss', "app/assets/stylesheets/#{file_name}.css.scss"
+          copy_file 'stylesheet.css.scss', "#{style_path}/stylesheets/#{style_name}.css.scss"
         else
           template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
-          copy_file 'stylesheet.css', "app/assets/stylesheets/#{file_name}.css"
+          copy_file 'stylesheet.css', "#{style_path}/stylesheets/#{style_name}.css"
         end
         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
         copy_file 'error_messages_helper.rb', 'app/helpers/error_messages_helper.rb'
